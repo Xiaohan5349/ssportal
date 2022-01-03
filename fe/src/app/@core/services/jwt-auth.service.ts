8d007bc25cd79431d45d0acf7f0936ffc1d97c6d
Chaopeng <you@example.com>
2022-01-03 10:56:56 -0500

    parse jwt error catch

--- fe/src/app/@core/services/jwt-auth.service.ts
+++ fe/src/app/@core/services/jwt-auth.service.ts
@@ -164,18 +164,20 @@ export class JwtAuthService {
     }
 
     parseJwt (jwt) {
-        console.log(jwt);
         const base64Url = jwt.split('.')[1];
-        console.log(base64Url);
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        console.log(base64);
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        console.log(jsonPayload);
-        const jwtOut = JSON.parse(jsonPayload);
-        this.ls.setItem(this.APP_USER, jwtOut);
-        console.log(jwtOut);
+        try {
+            const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                  }).join(''));
+                  console.log(jsonPayload);
+                  const jwtOut = JSON.parse(jsonPayload);
+                  this.ls.setItem(this.APP_USER, jwtOut);
+                  console.log(jwtOut);          
+        }
+        catch (err){
+            this.router.navigateByUrl('/logout');
+        }
     }
     cleanUserAndToken() {
         this.ls.setItem(this.JWT_TOKEN, null);
