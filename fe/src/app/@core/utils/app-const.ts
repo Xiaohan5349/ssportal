49046b30b4344cb28bbc6dfccca03197327ca9a9
Lee <lee@indigoconsulting.com>
2022-10-19 15:15:50 -0400

    fix: change FE from qa to prod

--- fe/src/app/@core/utils/app-const.ts
+++ fe/src/app/@core/utils/app-const.ts
@@ -1,12 +1,11 @@
 export class AppConst {
 
-// bbb
-  public static SERVER_PATH = 'https://mfaqa.bedbath.com/sso';
+// bbbqa_to_bbbprod
+  public static SERVER_PATH = 'https://mfa.bedbath.com/sso';
 //   public static SERVER_PATH = 'http://localhost:8080/sso';
 //   public static SERVER_PATH = 'http://172.24.160.53:8080/sso';
 
-  public static LOGIN_PATH = 'https://ssoqa.bedbath.com/idp/startSSO.ping?PartnerSpId=https%3A%2F%2Fssoqa.bedbath.com';
-
+  public static LOGIN_PATH = 'https://sso.bedbath.com/idp/startSSO.ping?PartnerSpId=https%3A%2F%2Fsso.bedbath.com';
 
   public static JWT_STORAGE_NAME = 'SSPORTAL_JWT_TOKEN'
   public static APP_USER_STORAGE_NAME = 'SSPORTAL_APP_USER'
